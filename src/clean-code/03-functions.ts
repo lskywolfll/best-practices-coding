b184ab2d6b1897127bccf10eaf421fa47b21966a
Rene <renealejandrosanchezmorales@gmail.com>
2022-04-26 19:31:02 -0400

    REFACTOR CODE INSIDE FUNCTIONS

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,37 +1,48 @@
 (() => {
-    function getMovieById( movieId: string ) {
-        console.log({ movieId });
-    }
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
 
-    function getAllMovieActors( movieId: string ) {
-        console.log({ movieId });
-    }
+  function getAllMovieActors(movieId: string) {
+    console.log({ movieId });
+  }
 
-    function getBioByActor( ActorId: string ) {
-        console.log({ ActorId });
-    }
+  function getBioByActor(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === "fernando") return false;
+
+    console.log("Crear actor");
+    return true;
+  }
+
+  // Continuar
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): number => {
+    if (isDead) return 1500;
+    if (isSeparated) return 2500;
     
-    interface Movie {
-        cast: string[];
-        description: string;
-        rating: number;
-        title: string;
-    }
-
-    function createMovie({title ,description, rating, cast}: Movie) {
-        console.log({ title, description, rating, cast });
-    }
-
-    // Crea un nuevo actor
-    function createActor( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( fullName === 'fernando' ) return false;
-
-        console.log('Crear actor');
-        return true;        
-    }
-
+    return isRetired ? 3000 : 4000;
+  };
 })();
