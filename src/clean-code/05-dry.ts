cef065a8fa2e8bbe449b97bb9c944e5609d2015c
Rene <renealejandrosanchezmorales@gmail.com>
2022-04-26 21:06:26 -0400

    refactor code

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,6 +7,24 @@ class Product {
         public price: number = 0,
         public size: Size = '',
     ){}
+
+    isProductReady(): boolean {
+        
+        for( const key in this ) {
+            switch( typeof this[key] ) {
+                case 'string':
+                    if ( (<string><unknown>this[key]).length <= 0 ) throw Error(`${ key } is empty`);
+                break;
+                case 'number':
+                    if ( (<number><unknown>this[key]) <= 0 ) throw Error(`${ key } is zero`);
+                break;
+                default:
+                    throw Error(`${ typeof this[key] } is not valid`);
+            }
+        }
+
+        return true;
+    }
     
     toString(){
         // if(this.name.length <= 0) throw Error('name is empty');
