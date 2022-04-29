34fccb2004e011debc98ea10b79ff036a2691bf8
Rene <renealejandrosanchezmorales@gmail.com>
2022-04-28 21:57:53 -0400

    added bad code to create a solution

--- src/clean-code/06-clases-a.ts
+++ src/clean-code/06-clases-a.ts
@@ -1,7 +1,6 @@
 (() => {
-
   // No Aplicando el principio de responsabilidad unica
-  
+
   type Gender = "M" | "F";
 
   class Person {
@@ -12,6 +11,48 @@
     ) {}
   }
 
-  const newPerson = new Person("Fernando", "M", new Date("1985-10-21"));
-  console.log({ newPerson });
+  class User extends Person {
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public rol: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    creackCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      rol: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, rol, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    "/usr/home",
+    "/home",
+    "fernando@gmail.com",
+    "admin",
+    "Fernando",
+    "M",
+    new Date("1985-10-21")
+  );
+
+  console.log({ userSettings })
 })();
